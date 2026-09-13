9b4c05c7564753b93c64e1921a7b4cb10c4e1477
Konstantin Dolbilov <konfrontend@gmail.com>
2026-09-13 20:22:13 +0200

    Import cn from lib/utils in the context menu.

--- apps/desktop/src/renderer/src/components/ui/context-menu.tsx
+++ apps/desktop/src/renderer/src/components/ui/context-menu.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { cn } from 'cn';
+import { cn } from '@/lib/utils';
 import { ChevronRightIcon, CircleIcon } from 'lucide-react';
 import { ContextMenu as ContextMenuPrimitive } from 'radix-ui';
 
